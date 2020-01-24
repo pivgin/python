5d846b318924f12b800a8517a7571f7e2793ae99
pivgin <pivgin@gmail.com>
2020-01-24 19:20:55 +0800

    Game guess the number (4 level) users added, game finished

--- hello_world/guess_game.py
+++ hello_world/guess_game.py
@@ -12,25 +12,43 @@ print('------------||------------')
 print()
 number = random.randint(1, 100)
 input_number = 0
+
 tries = 0
 levels = {1: 15, 2: 10, 3: 5, 4: 3, 5: 1}
 level = int(input('Please select the level (from 1 till 5 where 5 is hardest): '))
 max_tries = levels[level]
+
+users_count = int(input('Input number of users: '))
+users = []
+for i in range(users_count):
+    user_name = input('Input name for user {}: '.format(i))
+    users.append(user_name)
+print(users)
 #print(number)
-while input_number != number:
+
+is_winner = False
+winner_name = None
+
+while not is_winner:
     tries += 1
     if tries > max_tries:
-        print('You are looser!')
+        print('All users are loosers!')
         break
     print('{}th try'.format(tries))
-    input_number = int(input('Please input number'))
-    if input_number > number:
-        print('Go lower')
-    elif input_number < number:
-        print('Go higher')
+    for user in users:
+        print('{} user turn'.format(user))
+        input_number = int(input('Please input number'))
+        if input_number == number:
+            is_winner = True
+            winner_name = user
+            break
+        elif input_number > number:
+            print('Go lower')
+        else:
+            print('Go higher')
 else:
     print('You tried {} times'.format(tries))
-    print('Congrats! {} is correct number!'.format(input_number))
+    print('Congrats, {}! {} is correct number!'.format(winner_name, input_number))
 
 
 
