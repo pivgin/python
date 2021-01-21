b230c922c8750c1f13bee23a7d940348b0066a74
pivgin <pivgin@gmail.com>
2021-01-21 15:33:03 +0300

    first iteration completed, starting to work with problem elements

--- hello_world/vaccine.py
+++ hello_world/vaccine.py
@@ -20,14 +20,76 @@ print()
 # list_of_planets_ = [input('Input list of planets space separated')]
 # print(list_of_planets_)
 
-number_of_planets = int(input('Input number of planets '))
-# if number_of_planets.isdigit() (
-list_of_planets_ = []
-i = 0
-while i < number_of_planets:
-    list_of_planets_.append(int(input('Input capacity of planet ')))
-    i += 1
-print(list_of_planets_)
+# number_of_planets = int(input('Input number of planets '))
+# # if number_of_planets.isdigit() (
+# list_of_planets_ = []
+# i = 0
+# while i < number_of_planets:
+#     list_of_planets_.append(int(input('Input capacity of planet ')))
+#     i += 1
+# print(list_of_planets_)
+
+# list_of_planets_ = [5, 2, 4, 3, 5, 6, 2]
+list_of_planets_ = [5, 2, 4, 3, 5, 6, 2, 2]
+max_value = max(list_of_planets_)
+print(max_value)
+max_list_positions = [i for i, j in enumerate(list_of_planets_) if j == max_value]
+print(max_list_positions)
+copy_list_planets = list_of_planets_.copy()
+print(copy_list_planets)
+# slit by 3 elements
+split_list = []
+for i in range (0, len(copy_list_planets),3):
+    split_list.append(copy_list_planets[i : i+3])
+print(split_list)
+print(split_list[0])
+print(len(split_list))
+best_list = []
+increment = 0
+# for i in range (0,len(split_list[0])):
+for j in range(0, len(split_list)-1):
+    if split_list[j][0] + split_list[j][2] > split_list[j][1]:
+        best_list.append(0+increment)
+        best_list.append(2+increment)
+    else:
+        best_list.append(1)
+    print(best_list)
+    increment += 3
+
+print(increment)
+print ('-----------last part---------')
+last_mass_number = len(split_list)-1
+print (last_mass_number)
+print(increment)
+last_list = []
+last_list = split_list[last_mass_number].copy()
+if len(last_list) == 1:
+    best_list.append(0+increment)
+elif len(last_list) == 2:
+    max_last_list_positions = [n for n, m in enumerate(last_list) if m == max(last_list)]
+    for i in range (0,len(max_last_list_positions)):
+        max_last_list_positions[i] += increment
+        best_list.append(max_last_list_positions[i])
+else:
+    if (split_list[last_mass_number][0] + split_list[last_mass_number][2]) > (split_list[last_mass_number][1]):
+        best_list.append(0+increment)
+        best_list.append(2+increment)
+    else:
+        best_list.append(1+increment)
+print(best_list)
+problem_elements = []
+
+for i in range (1,len(best_list)-1):
+    if ((best_list[i] - best_list[i+1]) == -1) or ((best_list[i-1] - best_list[i]) == -1):
+        problem_elements.append(best_list[i])
+
+if best_list[len(best_list)-1] - best_list[len(best_list)-2] == 1:
+    problem_elements.append(best_list[len(best_list) - 1])
+
+print(problem_elements)
+
+
+
 
 
 
