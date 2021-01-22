8c283872fe598f267021c5fed3164ee1262a13e5
pivgin <pivgin@gmail.com>
2021-01-22 14:29:25 +0300

    at least something is working but with kostyl, remarks added

--- hello_world/vaccine.py
+++ hello_world/vaccine.py
@@ -32,10 +32,10 @@ print()
 # print(list_of_planets_)
 
 # Program itself
-# list_of_planets_ = [5, 2, 4, 3, 5, 6, 2]
-#list_of_planets_ = [4, 8, 5, 2]
-#list_of_planets_ = [4, 2, 3, 4]
 list_of_planets_ = [5, 2, 4, 3, 5, 6, 2]
+# list_of_planets_ = [4, 8, 5, 2]
+# list_of_planets_ = [4, 2, 3, 4]
+# list_of_planets_ = [5, 2, 4, 3, 5, 6, 2, 2]
 # checking for max values
 max_value = max(list_of_planets_)
 # print('Max value is', max_value)
@@ -102,7 +102,7 @@ print('List of positions of problem elements (problem_elements): ', problem_elem
 # priority of sections of thirds
 priority_list = []
 temp_sum = 0
-for i in range(0, len(split_list)-1):
+for i in range(0, len(split_list)-1):                   # TODO looks like problem. Needed to be rewritten to check only problem sections
     temp_sum = int(split_list[i][0] + split_list[i][2])
     if temp_sum > split_list[i][1]:
         priority_list.append(temp_sum)
@@ -123,23 +123,47 @@ third_priority = priority_list.index(max(priority_list))
 # print(third_priority)
 high_priority_list = split_list[third_priority]
 print('high_priority_list: ', high_priority_list)
-# TODO add if verification for case with one max trio
+# TODO add if verification for case with one max trio           almost done
 max_priority_list = []
 max_priority_list = [n for n, m in enumerate(priority_list) if m == max(priority_list)]
 print('Positions of best trios (max_priority_list):', max_priority_list)
+if len(max_priority_list) > 1:
+    for i in range(0, len(max_priority_list)-1):
+        if (split_list[i][1] + split_list[i+1][0]) < (split_list[i][2] + split_list[i+1][1]):       #TODO to make universal
+            best_list.remove(split_list[i+1][0])     #TODO to make universal
+        else:
+            best_list.remove(split_list[i][2])
+else:
+    print('Only on trio with max priority exists (third_priority): ', third_priority)
+    # TODO this is dead and for now needed to be updated
 
-for i in range(0, len(max_priority_list)-1):
-    if (split_list[i][1] + split_list[i+1][0]) > (split_list[i][2] + split_list[i+1][1]):       #TODO to make universal
-        best_list.remove(3)     #TODO to make universal
-# # adding increment to address element in best_list by value
-# third_increment = 0
-# if third_priority == 0:
-#     third_increment = 0
-# else:
-#     third_increment = third_priority + 2
-# # for i in range (0, 2):
-# #     if (i + third_increment) in best_list:
-# #         print('ok')
-# #     else:
-# #         best_list.remove(int(i + third_increment))
-# # print(best_list)
+print('UPD best list (best_list): ', best_list)
+problem_elements.clear()
+for i in range(1, len(best_list) - 1):
+    if ((best_list[i] - best_list[i + 1]) == -1) or ((best_list[i - 1] - best_list[i]) == -1):
+            problem_elements.append(best_list[i])
+
+if best_list[len(best_list) - 1] - best_list[len(best_list) - 2] == 1:
+    problem_elements.append(best_list[len(best_list) - 1])
+
+print('UPD List of positions of problem elements (problem_elements): ', problem_elements)
+# Trying to add resolvment for new problem list
+if len(problem_elements) > 1:
+    for i in problem_elements:
+        if (copy_list_planets[i-1] + copy_list_planets[i + 1]) > (copy_list_planets[i]):  # TODO to make universal as no such element present as there is only 1 in last section
+            best_list.remove(i)             #TODO in previous if it actually should be like this "> (copy_list_planets[i] + split_list[i + 2])"
+            break
+        else:
+            best_list.remove(i+1)
+print('UPD Best planets to visit (best_list): ', best_list)
+#TODO this is kastyl' try to rewrite
+#best_list.reverse()
+addition_to_best_list = []
+for i in range(0, len(best_list)-1):
+    if not best_list.count(best_list[i] + 2):
+        addition_to_best_list.append(best_list[i]+2)
+best_list = best_list + addition_to_best_list
+result = 0
+for i in best_list:
+    result += copy_list_planets[i]
+print('Result (result): ', result)
